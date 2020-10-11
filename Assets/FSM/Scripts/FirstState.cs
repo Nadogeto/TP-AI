ed165f2e86698e55e631d50d0450976747383144
Nadjet Tobbal <nadjetobbal@gmail.com>
2020-10-11 15:55:15 +0200

    FSM part1(update)

---
+++ Assets/FSM/Scripts/FirstState.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using State;
+
+public class FirstState : State<AI>
+{
+
+
+    public override void EnterState(AI _owner)
+    {
+    }
+
+    public override void ExitState(AI _owner)
+    {
+    }
+
+    public override void UpdateState(AI _owner)
+    {
+    }
+}
