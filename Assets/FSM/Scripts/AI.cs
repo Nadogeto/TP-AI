018518dc56c22c5ee07560058307d3e4623c9b25
Nadjet Tobbal <nadjetobbal@gmail.com>
2020-10-11 16:18:00 +0200

    Basic FSM (second state)

--- Assets/FSM/Scripts/AI.cs
+++ Assets/FSM/Scripts/AI.cs
@@ -1,18 +1,39 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using State;
 
 public class AI : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public bool switchState = false;
+    public float gameTimer;
+    public int seconds = 0;
+
+    public StateMachine<AI> stateMachine { get; set; }
+
+    private void Start()
     {
-        
+        stateMachine = new StateMachine<AI>(this);
+        stateMachine.ChangeState(FirstState.Instance);
+        gameTimer = Time.time;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        if(Time.time > gameTimer + 1)
+        {
+            gameTimer = Time.time;
+            seconds++;
+            Debug.Log(seconds);
+        }
+
+        if(seconds == 5)
+        {
+            seconds = 0;
+            switchState = !switchState;
+        }
+
+        stateMachine.Update();
     }
+
 }
